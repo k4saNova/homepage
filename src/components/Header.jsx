e6f182e413129af9133f26a8ae4dd0c91a4ab000
k4saNova <_@gmail.com>
2019-12-04 07:18:39 +0900

    fix: minor bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,64 +13,71 @@ const renderTitle = (p) => {
     default: // home
         const image = imageList[Math.floor(Math.random() * imageList.length)];
         return (
-          <div className="columns is-vcentered">
-            <div className="column is-5">
-              <figure className="image is-square">
-                <img src={`${process.env.PUBLIC_URL}/top-image/${image}`} />
-              </figure>
-            </div>
-            <div className="column is-6 is-offset-1">
-              <h1 className="title is-3">Kohei Asano</h1>
-              <h2 className="subtitle is-4">
-                Graduate School of Information Sciences<br />Tohoku University<br />
-                Chun Laboratry: Doctor student
-              </h2>
-	      <p>
-		<a href="#" target="_blank">About me (CV)</a>
-	      </p>
-            </div>
-          </div>            
+            <div className="columns is-vcentered">
+              <div className="column is-5">
+                <figure className="image is-square">
+                  <img src={`${process.env.PUBLIC_URL}/top-image/${image}`} />
+                </figure>
+              </div>
+              <div className="column is-6 is-offset-1">
+                <h1 className="title is-3">Kohei Asano</h1>
+                <h2 className="subtitle is-4">
+                  Graduate School of Information Sciences<br />Tohoku University<br />
+                  Chun Laboratry: Doctor student
+                </h2>
+	        <p>
+		  <a href="#" target="_blank">About me (CV)</a>
+	        </p>
+              </div>
+            </div>            
         );
     }
 };
 
-const Header = (props) => {
+const Navbar = (props) => {
     const [burger, setBurger] = useState(false);
     return (
-	<section className="hero is-dark">
-          <div className="hero-head">
-            <nav className="navbar is-dark">
-              <div className="container">
-	        <div className="navbar-brand">
-                  <span className={burger ? "navbar-burger burger is-active" : "navbar-burger burger"}
-                        onClick={() => setBurger(!burger)}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </span>
-	        </div>
-	        <div className={burger ? "navbar-menu is-active" : "navbar-menu"}>
-                  <div className="navbar-end">
-	            <div className="tabs is-right">
-	              <ul>
-                        {pageTypes.map(
-                            (p) => <li key={p+".li"}
-                                       className={props.page===p ? "is-active" : ""}>
-                                     <a onClick={() => props.changePage(p)}>
-                                       <span className="icon">
-			                 <i className={pageProps.icon[p]}></i>
-                                       </span>
-                                       {pageProps.title[p]}
-                                     </a>
-                                   </li>
-                        )}
-	              </ul>
-	            </div>
+        <div className="hero-head">
+          <nav className="navbar is-dark">
+            <div className="container">
+	      <div className="navbar-brand">
+                <span className={burger ? "navbar-burger burger is-active" : "navbar-burger burger"}
+                      onClick={() => setBurger(!burger)}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+	      </div>
+	      <div className={burger ? "navbar-menu is-active" : "navbar-menu"}>
+                <div className="navbar-end">
+	          <div className="tabs is-right">
+	            <ul>
+                      {pageTypes.map(
+                          (p) => <li key={p+".li"}
+                          className={props.page===p ? "is-active" : ""}>
+                   <a onClick={() => props.changePage(p)}>
+                     <span className="icon">
+		       <i className={pageProps.icon[p]}></i>
+                     </span>
+                     {pageProps.title[p]}
+                   </a>
+                 </li>
+                      )}
+	            </ul>
 	          </div>
-                </div>
+	        </div>
               </div>
-            </nav>
-          </div>
+            </div>
+          </nav>
+        </div>        
+    );
+};
+
+const Header = (props) => {
+    return (
+	<section className="hero is-dark">
+          <Navbar page={props.page}
+                  changePage={(val) => props.changePage(val)}/>
           <div className="hero-body">
             <div className="container has-text-centered">
               {renderTitle(props.page)}
